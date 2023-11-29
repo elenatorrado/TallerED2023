bf58f0fefeef8d7142e188010b0e457040245d3a
Studium Formacion <elenatorrado0912@gmail.com>
2023-11-29 13:03:38 +0100

    First commit

---
+++ src/es/studium/holamundo/HolaMundo.java
@@ -0,0 +1,14 @@
+package es.studium.holamundo;
+
+public class HolaMundo
+{
+	public static void main(String[] args) {
+
+        System.out.println("Hola, mundo!");
+        System.out.print("Buenas!");
+        System.out.println();
+        System.out.println();
+        System.out.println("Buenos dias");
+
+    }
+}
